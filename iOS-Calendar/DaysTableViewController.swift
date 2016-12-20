e789a4b0104bd07aaf8c130a395f6694163341dc
Kaustubh <kaustubh.kesarkar@gmail.com>
2016-12-19 22:21:24 -0800

    Adds data for days and single day for it's respective table view controller.

--- iOS-Calendar/DaysTableViewController.swift
+++ iOS-Calendar/DaysTableViewController.swift
@@ -11,4 +11,17 @@ import UIKit
 
 class DaysTableViewController : UITableViewController {
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 31
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Basic")!
+        cell.textLabel?.text = "\(indexPath.row + 1)"
+        return cell
+    }
 }
--- iOS-Calendar/SingleDayTableViewController.swift
+++ iOS-Calendar/SingleDayTableViewController.swift
@@ -11,4 +11,19 @@ import UIKit
 
 class SingleDayTableViewController : UITableViewController {
     
+    var events: [String] = Array()
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return events.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Basic")!
+        cell.textLabel?.text = events[indexPath.row]
+        return cell
+    }
 }
